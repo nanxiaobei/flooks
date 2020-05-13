bb11d0e10ba9250b71cc9ab5e921fe677f2a2571
nanxiaobei <nanxiaobei@gmail.com>
2020-05-13 20:54:43 +0800

    update tests

--- src/index.test.js
+++ src/index.test.js
@@ -40,45 +40,46 @@ test('render', (done) => {
       add();
     },
   });
-  const outer = use({
-    addOutErr() {
+  const errModel = use({
+    errPayload() {
       const { add } = counter();
       add();
       use([]);
     },
   });
   const useCounter = counter;
-  const useOuter = outer;
+  const useErrModel = errModel;
 
   const ErrKeys = () => {
     useCounter({});
-    return null;
+    return <div />;
   };
   expect(() => {
     shallow(<ErrKeys />);
   }).toThrow();
 
   const Counter = () => {
-    const { count } = useCounter(['count']);
-    const { add, addAsync } = useCounter([]);
-    const { addOutErr } = useOuter();
+    const { count } = useCounter();
+    const { add } = useCounter(['add']);
+    const { addAsync } = useCounter([]);
+    const { errPayload } = useErrModel();
+
     return (
       <>
         <p>{count}</p>
         <button id="add" onClick={add} />
         <button id="addAsync" onClick={addAsync} />
-        <button id="addOutErr" onClick={addOutErr} />
+        <button id="errPayload" onClick={errPayload} />
       </>
     );
   };
   const wrapper = mount(<Counter />);
 
   wrapper.find('#add').simulate('click');
-  withoutCheck(() => {
-    wrapper.find('#addAsync').simulate('click');
-  });
+  wrapper.find('#addAsync').simulate('click');
+
   expect(() => {
-    wrapper.find('#addOutErr').simulate('click');
+    wrapper.find('#errPayload').simulate('click');
   }).toThrow();
 
   setTimeout(() => {
